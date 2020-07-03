1f0be8507e393f50169a85e404f92c6874cc553a
breno39 <64810645+breno39@users.noreply.github.com>
2020-07-03 17:13:17 -0300

    componente wakanda-tribeslist criado

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { CompartilhadoModule } from '../compartilhado/compartilhado.module';
+import { WakandaTribeslistComponent } from './wakanda-tribeslist/wakanda-tribeslist.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CompartilhadoModule
   ],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, WakandaTribeslistComponent],
   exports: [HomeComponent]
 })
 export class HomeModule { }

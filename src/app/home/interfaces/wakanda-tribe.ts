f4dc1d0ae53dc1720e34cd121edf0dc408ad200e
breno39 <64810645+breno39@users.noreply.github.com>
2020-07-03 17:49:05 -0300

    criada interface wakanda-tribe

---
+++ src/app/home/interfaces/wakanda-tribe.ts
@@ -0,0 +1,6 @@
+export interface WakandaTribe {
+    nome: String,
+    descricao: String,
+    simbolo: ImageData,
+    dataDeInicio: Date
+}
--- src/app/home/services/wakanda-tribeslist.service.ts
+++ src/app/home/services/wakanda-tribeslist.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { WakandaTribe } from '../interfaces/wakanda-tribe';
 
+const API_URL= environment.apiUrl
 @Injectable({
   providedIn: 'root'
 })
 export class WakandaTribeslistService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getTribos() {
+    return this.http.get<WakandaTribe[]>(API_URL);
+  }
 }

84a1ddee998a0d4e4dfdd0606489af8f67204a3b
breno39 <64810645+breno39@users.noreply.github.com>
2020-07-05 20:58:47 -0300

    criado componente wakandertribedetail e modulo wakander

---
+++ src/app/wakander/wakander-tribe-detail/wakander-tribe-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wakander-tribe-detail',
+  templateUrl: './wakander-tribe-detail.component.html',
+  styleUrls: ['./wakander-tribe-detail.component.css']
+})
+export class WakanderTribeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

a59e37ba127d933a0a4a9ce8b49a12410798e7d2
breno39 <64810645+breno39@users.noreply.github.com>
2020-07-05 21:27:10 -0300

    adicionado header em wakandertribedetailcomponent

--- src/app/wakander/wakander.module.ts
+++ src/app/wakander/wakander.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { WakanderTribeDetailComponent } from './wakander-tribe-detail/wakander-tribe-detail.component';
+import { CompartilhadoModule } from '../compartilhado/compartilhado.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CompartilhadoModule
   ],
   declarations: [WakanderTribeDetailComponent]
 })

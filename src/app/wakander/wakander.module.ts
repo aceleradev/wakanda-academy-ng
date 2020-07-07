8c3ff8fdd2768d0a934cfe753b9d37b1ac389a34
breno39 <64810645+breno39@users.noreply.github.com>
2020-07-07 15:28:30 -0300

    componente wakanderTribeSkillList criado

--- src/app/wakander/wakander.module.ts
+++ src/app/wakander/wakander.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { WakanderTribeDetailComponent } from './wakander-tribe-detail/wakander-tribe-detail.component';
 import { CompartilhadoModule } from '../compartilhado/compartilhado.module';
+import { WakanderTribeSkillListComponent } from './wakander-tribe-detail/wakander-tribe-skill-list/wakander-tribe-skill-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CompartilhadoModule
   ],
-  declarations: [WakanderTribeDetailComponent]
+  declarations: [WakanderTribeDetailComponent, WakanderTribeSkillListComponent]
 })
 export class WakanderModule { }

170fa8333c68b0ad4f20490332889ffa2ca49432
breno39 <64810645+breno39@users.noreply.github.com>
2020-07-14 20:09:35 -0300

    adicionado proxy

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const https = require('https');
+const axios = require('axios');
+const environment = require('./src/environments/environment');
+const nomeApp = process.env.npm_package_name;
+const app = express();  
+
+app.use(express.json());
+
+app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+ 
+app.post('/wakanda/app/v1/user/create', (req, res) => {
+    axios.post(environment.apiUrl, req.body)
+        .then((clientRespose) => {
+            res.send(clientRespose.data);
+        }, (error) => {
+            res.status(error.response.status).send(error.response.data);
+        });
+});
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+});
+
+
+ 
+app.listen(process.env.PORT || 8080);
